f56861cf5997dd0e6bc6c5b91d328d1266fbf0fd
Howard Pinsley <hpinsley@gmail.com>
2015-06-30 19:53:52 -0400

    Zip example

--- src/RxSimple.ts
+++ src/RxSimple.ts
@@ -16,8 +16,8 @@ export class RxSimple {
 	
 	fromZip() {
 		var s1 = Rx.Observable.range(1,5);
-		var s2 = Rx.Observable.range(1,5);
-		var s3 = Rx.Observable.zip(s1, s2, (v1,v2) => v1+v2);
+		var s2 = Rx.Observable.range(1,10);
+		var s3 = Rx.Observable.zip(s1, s2, (v1,v2) => [v1,v2]);
 		
 		s3.subscribe(v => console.log(v));
 	}
